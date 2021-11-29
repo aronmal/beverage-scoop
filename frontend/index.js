586730ac4d81672c0851ae25728470bda90dcb73
aronmal <aron@mal-noh.de>
2021-11-29 10:58:19 +0100

    Lots of bubbles, nice and random

--- frontend/index.js
+++ frontend/index.js
@@ -8,30 +8,31 @@ for (var i = 0; i < svgdiv.length; ++i) {
 }
 
 async function makeBubbles(item) {
-    console.log(offsetInterval)
     setInterval(() => {
-        const offsetTimout = Math.round(randomNum(0,5000))
+        const offsetTimout = Math.round(randomNum(0,2000))
         setTimeout(() => createBubble(item), offsetTimout)
-    }, 5000);
+    }, 1000);
 }
 
 function createBubble(item) {
-    const size = `${Math.round((randomNum(0.5,2)) * 100) / 100}rem`
+    const size = `${Math.round((randomNum(0.5,1.5)) * 100) / 100}rem`
     const left = `${Math.round(randomNum(10,90))}%`
-    console.log(size)
-    console.log(left)
+    console.log(left,size)
     const bubblediv = document.createElement('div')
     const bubble = document.createElement('div')
     bubblediv.classList.add('bubble-div')
     bubblediv.style.height = size
     bubblediv.style.width = size
     bubblediv.style.left = left
+    bubblediv.style.animation = 'bubblesY 5s linear'
+    bubblediv.style.animationFillMode = 'forwards'
     bubble.classList.add('bubble')
     bubble.style.height = size
     bubble.style.width = size
+    bubble.style.animation = 'bubblesX .4s ease-in-out alternate infinite'
     bubblediv.appendChild(bubble)
     item.parentNode.insertBefore(bubblediv, item)
-    setTimeout(() => bubblediv.remove(), 6000)
+    setTimeout(() => bubblediv.remove(), 5000)
 }
 
 function randomNum(min, max) {
