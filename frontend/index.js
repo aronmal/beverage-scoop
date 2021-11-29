5594eb58b42945f9d2e21c59a5614a906a92718f
aronmal <aron@mal-noh.de>
2021-11-29 10:00:13 +0100

    More random bubbles

--- frontend/index.js
+++ frontend/index.js
@@ -4,22 +4,34 @@ const svgdiv = content.getElementsByClassName('wave-trans')
 
 
 for (var i = 0; i < svgdiv.length; ++i) {
-    createbubble(svgdiv[i])
+    makeBubbles(svgdiv[i])
 }
 
-function createbubble(item) {
+async function makeBubbles(item) {
+    console.log(offsetInterval)
+    setInterval(() => {
+        const offsetTimout = Math.round(randomNum(0,5000))
+        setTimeout(() => createBubble(item), offsetTimout)
+    }, 5000);
+}
+
+function createBubble(item) {
     const size = `${Math.round((randomNum(0.5,2)) * 100) / 100}rem`
+    const left = `${Math.round(randomNum(10,90))}%`
     console.log(size)
+    console.log(left)
     const bubblediv = document.createElement('div')
     const bubble = document.createElement('div')
     bubblediv.classList.add('bubble-div')
     bubblediv.style.height = size
     bubblediv.style.width = size
+    bubblediv.style.left = left
     bubble.classList.add('bubble')
     bubble.style.height = size
     bubble.style.width = size
     bubblediv.appendChild(bubble)
     item.parentNode.insertBefore(bubblediv, item)
+    setTimeout(() => bubblediv.remove(), 6000)
 }
 
 function randomNum(min, max) {
