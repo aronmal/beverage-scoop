18b97d6fee91ea2af40dfff4a80998661a86e511
aronmal <aron@mal-noh.de>
2021-11-30 10:18:09 +0100

    Use express.static('public')

--- nodejs/app.js
+++ nodejs/app.js
@@ -10,16 +10,9 @@ var newConfigFileData
 
 // Start listening on port 5000
 app.listen(5000, () => console.log('Server running on: http://localhost:5000'))
-// app.use(express.static('public'))
+app.use(express.static('public'))
 app.use(express.json())
 app.use(cors());
-
-// NGINX Replacement
-app.get('/', (req,res) => {res.sendFile(path.join(__dirname, 'src/index.html'))})
-app.get('/index.html', (req,res) => {res.sendFile(path.join(__dirname, 'src/index.html'))})
-app.get('/favicon.ico', (req,res) => {res.sendFile(path.join(__dirname, 'src/favicon.ico'))})
-app.get('/index.js', (req,res) => {res.sendFile(path.join(__dirname, 'src/index.js'))})
-app.get('/style.css', (req,res) => {res.sendFile(path.join(__dirname, 'src/style.css'))})
 
 // GET 'Hello World!'
 app.get('/api/hello', (req,res) => res.send('Hello World!'))
