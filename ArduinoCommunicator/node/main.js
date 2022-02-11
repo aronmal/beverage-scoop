1d61473db825d68a9fa05496bfcdeacfd90099c3
aronmal <aron@mal-noh.de>
2022-02-11 16:18:20 +0100

    Little cleanup

--- ArduinoCommunicator/node/main.js
+++ ArduinoCommunicator/node/main.js
@@ -1,58 +1,52 @@
 const serialport = require('serialport')
 // FIXME THIS LANGUAGE IS THE FUCKING SPAWN OF SATAN HIMSELF
-var Readline = serialport.parsers.Readline; // make instance of Readline parser
-// var parser = new Readline(); // make a new parser to read ASCII lines
-
-const parser = new Readline()
+const Readline = serialport.parsers.Readline; // make instance of Readline parser
+const parser = new Readline(); // make a new parser to read ASCII lines
 
 const userInput = "A"
 
-// const port = new serialPort('/dev/ttyACM0')
+const main = async () => {
+
+    // list serial ports:
+    const ports = await serialport.list().then(ports => ports.filter(port => port.manufacturer));
+
+    if (ports.length !== 1)
+        return;
+    const port = new serialport(ports[0].path ,{
+        baudRate: 9600,
+    });
+
+    // Read Line Parser
+    port.pipe(parser); // pipe the serial stream to the parser
+
+    // Handling Events
+    port.on('open', showPortOpen);
+    // Callback Functions
+    function showPortOpen() {
+        console.log('Port open. baudRate: ' + port.baudRate);
+    }
+
+    // Open errors will be emitted as an error event
+    port.on('error', function(err) {
+        console.log('Error: ', err.message)
+    })
+
+    // Read data that is available but keep the stream in "paused mode"
+    // parser.on('readable', function () {
+    //     console.log('Data:', port.read())
+    // })
+    // Switches the port into "flowing mode"
+    parser.on('data', console.log)
 
 
-// list serial ports:
-// serialport.list(function (err, ports) {
-//     ports.forEach(function (port) {
-//         console.log("Serial Device: " + port.comName);
-//     });
-// });
-
-// get port name from the command line:
-//var path = '/dev/cu.SLAB_USBtoUART' ;
-var path = '/dev/ttyACM0';
-
-var port = new serialport(path ,{
-    baudRate: 9600,
-});
-
-// Read Line Parser
-port.pipe(parser); // pipe the serial stream to the parser
-
-// Handling Events
-port.on('open', showPortOpen);
-// Callback Functions
-function showPortOpen() {
-    console.log('Port open. baudRate: ' + port.baudRate);
+    setTimeout(() => {
+        port.write(userInput, function(err) {
+            if (err) {
+                return console.log('Error on write: ', err.message)
+            }
+            console.log('message written')
+        })
+    }, 1000)
 }
 
-// Open errors will be emitted as an error event
-port.on('error', function(err) {
-    console.log('Error: ', err.message)
-})
-
-// Read data that is available but keep the stream in "paused mode"
-// parser.on('readable', function () {
-//     console.log('Data:', port.read())
-// })
-// Switches the port into "flowing mode"
-parser.on('data', console.log)
-
-
-setTimeout(() => {
-    port.write(userInput, function(err) {
-        if (err) {
-            return console.log('Error on write: ', err.message)
-        }
-        console.log('message written')
-    })
-}, 1000)
+main();
