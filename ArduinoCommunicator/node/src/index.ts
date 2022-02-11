42f47fa859685ffeb503485011163ba15dc82a66
aronmal <aron@mal-noh.de>
2022-02-11 17:37:39 +0100

    Working command prompt with Arduino responding

--- ArduinoCommunicator/node/src/index.ts
+++ ArduinoCommunicator/node/src/index.ts
@@ -1,9 +1,15 @@
 import SerialPort from 'serialport';
+import readline from 'readline';
+import colors from 'colors';
+colors.enable();
 
 const Readline = SerialPort.parsers.Readline; // make instance of Readline parser
 const parser = new Readline({ delimiter: '\r\n' }); // make a new parser to read ASCII lines
 
-const userInput = "A"
+const prompt = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
 
 const main = async () => {
 
@@ -20,11 +26,9 @@ const main = async () => {
     port.pipe(parser); // pipe the serial stream to the parser
 
     // Handling Events
-    port.on('open', showPortOpen);
-    // Callback Functions
-    function showPortOpen() {
-        console.log('Port open. baudRate: ' + port.baudRate);
-    }
+    port.on('open', () => {
+        console.log('[Info] '.cyan + 'Port open with baud rate: ' + port.baudRate);
+    });
 
     // Open errors will be emitted as an error event
     port.on('error', function(err) {
@@ -32,21 +36,31 @@ const main = async () => {
     })
 
     // Read data that is available but keep the stream in "paused mode"
-    // parser.on('readable', function () {
-    //     console.log('Data:', port.read())
-    // })
+    /* parser.on('readable', console.log) */
     // Switches the port into "flowing mode"
-    parser.on('data', console.log)
+    parser.on('data', data => {
+        console.log('[Serial] '.gray + data)
+        if (data !== 'Input A Command.')
+            return;
+        promptCommand(port);
+    })
 
+}
 
-    setTimeout(() => {
-        port.write(userInput, function(err) {
-            if (err) {
-                return console.log('Error on write: ', err.message)
-            }
-            console.log('message written')
-        })
-    }, 1000)
+function promptCommand(port: SerialPort) {
+    console.log('[Prompt] '.green + 'What command do you want to send? ')
+    prompt.question('[Prompt] '.green + 'Command: ', command => {
+        portWrite(port, command);
+        // prompt.close();
+    })
+}
+
+function portWrite(port: SerialPort, command: string) {
+    port.write(command, (err) => {
+        if (err)
+            return console.log('Error on write: ', err.message);
+        console.log('[System] '.cyan + 'Message `' + command + '` written')
+    })
 }
 
 main();
--- ArduinoCommunicator/sketch_SerialTest/sketch_SerialTest.ino
+++ ArduinoCommunicator/sketch_SerialTest/sketch_SerialTest.ino
@@ -1,6 +1,5 @@
 String input;
 
-
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600); //init Serial
@@ -10,23 +9,13 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   Serial.println("Input A Command.");
-  while(Serial.available() == 0){} //wait for inout
+  while (Serial.available() == 0) {
+    // Wait for Input Data
+  }
   input = Serial.readString();
-  Serial.print("Command was recieved: `" + input);
+  Serial.println("Command was recieved: `" + input + "");
+  if (input.startsWith("A")) {
+    Serial.println("'B'");
+  }
   //done.
 }
-
-/*
-
-
-input = Serial.readString(); //Read input
-    if(input.startsWith("JavaScript sucks balls dude")){
-      Serial.println("Based.");
-    } else if(input.startsWith("A")){
-      Serial.println("B");
-    } else{
-      Serial.println("Command not found; Input: " + input);
-    }
-
-
-*/
